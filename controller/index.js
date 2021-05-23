fdf4258cbb321be5418fae0bf6dbd6c5e7866719
Sachin <sachinkshuklaoo7@gmail.com>
2021-05-23 15:51:35 +0530

    base employees get all data route working

--- controller/index.js
+++ controller/index.js
@@ -2,5 +2,8 @@ const employeesData = require("../data/index.json");
 
 // 1. Get all employees data
 exports.getAllEmployees = async (req, res) => {
-    
+  res.status(200).json({
+    status: true,
+    ...employeesData,
+  });
 };
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const app = require("./app");
-const config = require("./config");
 
-const PORT = config.PORT || 8000;
+const PORT = 8000;
 
 app.listen(PORT, () =>
   console.log(`Server has started on port localhost:${PORT}`)
