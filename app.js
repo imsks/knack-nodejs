e20cddc0e0feea584a4a1ad0f8faa0e880e53aa1
Sachin <sachinkshuklaoo7@gmail.com>
2021-05-23 15:47:14 +0530

    base biolerplate code ready

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
+
+const employeeRoutes = require("./routes/index");
 
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(morgan("combined"));
+app.use(express.json());
 
-app.use("/api/v1/employee", globalServiceRouter);
-
-app.use("/api/v1/", (req, res) =>
-  res.json({
-    status: true,
-    message: "Test route working",
-  })
-);
+app.use("/api/v1/employee", employeeRoutes);
 
 module.exports = app;
--- controller/index.js
+++ controller/index.js
@@ -1,3 +1,5 @@
+const employeesData = require("../data/index.json");
+
 // 1. Get all employees data
 exports.getAllEmployees = async (req, res) => {
     
