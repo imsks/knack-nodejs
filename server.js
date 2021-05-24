1f965eacc9ca92771ccca7904d5146e6b28f7c0f
Sachin <sachinkshuklaoo7@gmail.com>
2021-05-24 14:04:34 +0530

    base implementation ready

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const app = require("./app");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" })
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () =>
   console.log(`Server has started on port localhost:${PORT}`)
